f0aa80423539827fb6bb1945c9d1d50b80c32af9
Tim Tully <tully_tim@yahoo.com>
2023-05-15 22:16:57 -0700

    latest

---
+++ src/neonutils.rs
@@ -0,0 +1,52 @@
+// https://github.com/sfackler/rust-postgres/issues/858
+
+use chrono::Utc;
+
+/// The postgres-crate does not provide a default mapping to fallback to String for all
+/// types: row.get is generic and without a type assignment the FromSql-Trait cannot be inferred.
+/// This function matches over the current column-type and does a manual conversion
+pub fn reflective_get(row: &postgres::Row, index: usize) -> String {
+    let column_type = row.columns().get(index).map(|c| c.type_().name()).unwrap();
+    println!("----- {}", column_type);
+    // see https://docs.rs/sqlx/0.4.0-beta.1/sqlx/postgres/types/index.html
+    let value = match column_type {
+        "bool" => {
+            let v: Option<bool> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "varchar" | "char(n)" | "text" | "name" => {
+            let v: Option<String> = row.get(index);
+            v
+        }
+        // "char" => {
+        //     let v: i8 = row.get(index);
+        // }
+        "int2" | "smallserial" | "smallint" => {
+            let v: Option<i16> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "int" | "int4" | "serial" => {
+            let v: Option<i32> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "int8" | "bigserial" | "bigint" => {
+            let v: Option<i64> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "float4" | "real" => {
+            let v: Option<f32> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "float8" | "double precision" => {
+            let v: Option<f64> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        "timestamp" | "timestamptz" => {
+            // with-chrono feature is needed for this
+            let v: Option<chrono::DateTime<Utc>> = row.get(index);
+            v.map(|v| v.to_string())
+        }
+        &_ => Some("CANNOT PARSE".to_string()),
+    };
+    value.unwrap_or("".to_string())
+}
