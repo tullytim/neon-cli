27e501f010ccc24d94b86a2b56600d5ae78c7ef5
Tim Tully <tully_tim@yahoo.com>
2023-05-17 10:06:13 -0700

    latest

--- src/networking.rs
+++ src/networking.rs
@@ -11,16 +11,6 @@ pub async fn do_http_get(url: String, neon_config: &NeonSession) -> Result<Strin
     .header("Accept", "application/json")
     .send().await;
     let response = body.expect("Failed to execute request.");
-    
     let body = response.text().await;
-    
-    //println!("body = {:?}", body);
     Ok(body.unwrap())
-    
-    /* 
-    let p = body.unwrap();
-    println!("p is {:?}", p);
-    Ok(p)
-    */
-
 }
